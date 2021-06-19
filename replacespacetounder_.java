7f79de422e5e434193b6be0a4025e08c556838e7
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-06-19 19:38:32 +0530

    Add files via upload

---
+++ replacespacetounder_.java
@@ -0,0 +1,16 @@
+package codewithaman;
+import java.util.Scanner;
+public class replacespacetounder_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the string");
+		String s=sc.next();
+	//	System.out.println("enter the charactor you want to replace");
+	//	char l= sc.next().charAt(0);
+		System.out.print(s.replace('m', 'n'));
+		
+	}
+
+}
